9a8f2f7793ff30eba172c70fb6ccab7168f9a89c
Sarthak Dayanand <sarthak.kc@gmail.com>
2016-11-03 11:21:17 +0000

    first commit from Mac

--- src/test/java/com/berenberg/bank/cukeTestWithArcheType/OnlineBookingStepDefs.java
+++ src/test/java/com/berenberg/bank/cukeTestWithArcheType/OnlineBookingStepDefs.java
@@ -30,9 +30,10 @@ public class OnlineBookingStepDefs {
 	
 	@Before("@OnlineBooking")
 	public void setup() {
+		System.out.println("OS is:"+System.getProperty("os.name"));
 		if(System.getProperty("os.name").contains("Windows"))
 			System.setProperty("webdriver.chrome.driver", "C:/Users/IBM_ADMIN/Documents/Automation/chromedriver.exe");
-		else
+		else if(System.getProperty("os.name").contains("Mac"))
 			System.setProperty("webdriver.chrome.driver", "/Users/sarthakdayanand/Documents/Automation/chromedriver");
 		driver = new ChromeDriver();
 		driver.get("https://better.legendonlineservices.co.uk/enterprise/account/login");
