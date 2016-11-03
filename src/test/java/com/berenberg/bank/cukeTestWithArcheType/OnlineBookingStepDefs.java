07bf533344dc4c18fe3fbd6e84c6f486f5b4d77e
Sarthak Windows <sarthak.dayanand@hotmail.co.uk>
2016-11-03 11:36:35 +0000

    changes from pc

--- src/test/java/com/berenberg/bank/cukeTestWithArcheType/OnlineBookingStepDefs.java
+++ src/test/java/com/berenberg/bank/cukeTestWithArcheType/OnlineBookingStepDefs.java
@@ -31,7 +31,7 @@ public class OnlineBookingStepDefs {
 	@Before("@OnlineBooking")
 	public void setup() {
 		System.out.println("OS is:"+System.getProperty("os.name"));
-		if(System.getProperty("os.name").contains("Windows"))
+		if(System.getProperty("os.name").contains("Windows: changed from pc"))
 			System.setProperty("webdriver.chrome.driver", "C:/Users/IBM_ADMIN/Documents/Automation/chromedriver.exe");
 		else if(System.getProperty("os.name").contains("Mac"))
 			System.setProperty("webdriver.chrome.driver", "/Users/sarthakdayanand/Documents/Automation/chromedriver");
