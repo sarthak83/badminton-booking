3407d3c9da2e91a9cc923771dc54898e6f89877c
Sarthak Windows <sarthak.dayanand@hotmail.co.uk>
2016-11-16 12:31:12 +0000

    commit to check Jenkins

--- src/test/java/com/berenberg/bank/cukeTestWithArcheType/RunCukesTest.java
+++ src/test/java/com/berenberg/bank/cukeTestWithArcheType/RunCukesTest.java
@@ -14,7 +14,7 @@ import cucumber.api.junit.Cucumber;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/",
-                 tags={"@TestBut"},
+                 tags={"@TestThen"},
                  plugin={"pretty",
                 		 "html:target/test-report",
                 		 "json:target/test-report.json",
